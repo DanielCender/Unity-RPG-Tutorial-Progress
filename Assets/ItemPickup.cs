794b37ae3dfd5f551fbedfcf56a6eb7addd8e840
Daniel Cender <danielcender@gmail.com>
2022-01-29 22:45:32 -0600

    finished up initial items and inventory structure video

---
+++ Assets/ItemPickup.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : Interactable
+{
+    public Item item;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        Pickup();
+    }
+
+    void Pickup()
+    {
+        Debug.Log("We are picking up " + item.name);
+
+        bool wasPickedUp = Inventory.instance.Add(item);
+
+        if(wasPickedUp)
+            Destroy(gameObject);
+    }
+}
